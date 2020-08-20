c2908be8329f936633fbf09146593f4bea40fb7f
BlackHeart <valepa007@gmail.com>
2020-08-20 21:43:20 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+// Базовая конфигурация
+module.exports = {
+  entry: './src/index.js', // главный файл
+  output: { // выходные данные
+    filename: "bundle.js", // имя файла
+    path: path.resolve(__dirname, 'dist') // куда сложить данные
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
