85e98051bf36838b5ca96e25445129dbfac41220
BlackHeart <valepa007@gmail.com>
2020-08-22 11:06:13 +0300

    Добален класс для панели конструтора (переписать на classes)

---
+++ src/classes/sidebar.js
@@ -0,0 +1,54 @@
+import {TitleBlock, TextBlock} from './block'
+
+export class Sidebar {
+  constructor(selector) {
+    this.$el = document.querySelector(selector)
+
+    this.init()
+  }
+
+
+  init() {
+    this.$el.addEventListener('submit', this.addBlock)
+    this.$el.innerHTML = this.template
+  }
+
+  get template() {
+    return [
+      block('text'),
+      block('title')
+    ].join('')
+  }
+
+  addBlock(event) {
+    event.preventDefault()
+    const type = event.target.name
+    const controlValue = event.target.value.value
+    const controlStyles = event.target.styles.value
+
+    //Переписать ан классы
+    const Constructor = type === 'text' ? TextBlock : TitleBlock
+
+    const newBlock = new Constructor(controlValue, {controlStyles})
+
+    event.target.value.value = ''
+    event.target.styles.value = ''
+  }
+
+}
+
+function block(type) {
+  return `
+    <form name="${type}">
+        <h5>${type}</h5>
+        <div class="form-group">
+            <input class="form-control form-control-sm" name="value" placeholder="value">
+        </div>
+         <div class="form-group">
+            <input class="form-control form-control-sm" name="styles" placeholder="styles">
+        </div>
+        <button type="submit" class="btn btn-primary btn-sm">Добавить</button>
+    </form>
+    <hr/>
+  `
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import {Site} from "./classes/site";
+
 import {model} from './model.js'
 import {templates} from "./templetes";
 import './styles/main.css'
+import {Sidebar} from "./classes/sidebar";
 
 const site = new Site('#site')
+
+new Sidebar('#panel')
 
 site.render(model)
 
