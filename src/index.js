03dbb56d034a9625da68d7e6e0b9028a2070ff83
BlackHeart <valepa007@gmail.com>
2020-08-22 18:12:26 +0300

    Добалена функция css упрощающая запись стилей (напсать функция перевода camelCase в строку)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import {Site} from "./classes/site";
 
 import {model} from './model.js'
-import {templates} from "./templetes";
 import './styles/main.css'
 import {Sidebar} from "./classes/sidebar";
 
--- src/model.js
+++ src/model.js
@@ -1,23 +1,38 @@
 import image from './assets/image.png'
 import {ImageBlock, TextBlock, TextColumnsBlock, TitleBlock} from './classes/block'
+import {css} from "./utils";
 
 export const model = [
   new TitleBlock('Конструктор сайтов на чистои JavaScript', {
     tag: 'h3',
-    styles: 'background: #63a7f0; color: #fff; text-align: center; padding: 1.5rem'
+    styles: css({
+      background: '#63a7f0',
+      color: '#fff',
+      'text-align': 'center',
+      padding: '1.5rem'
+    })
   }),
   new ImageBlock(image, {
     alt: 'my image',
     imageStyles: 'width: 100%; height: auto'
   }),
   new TextBlock('Новости недели', {
-    styles: 'background: #fafaff; color: black; padding: 10px'
+    styles: css({
+      background: '#fafaff',
+      color: 'black',
+      'text-align': 'center',
+      padding: '10px'
+    })
   }),
   new TextColumnsBlock([
     'Ninja поставил 420 069 долларов на аукционе для получения собственного скина в Fall Guys',
     'Отличный перезапуск или просто ностальгический трюк? Мнения критиков о новой Battletoads разделились ',
     'Epic Games обозначила примерную дату начала продаж PS5 и Xbox Series X'
   ], {
-    styles: 'background: #fafaff; padding: 1rem'
+    styles: css({
+      background: '#fafaff',
+      color: 'black',
+      padding: '1rem'
+    })
   }),
 ]
